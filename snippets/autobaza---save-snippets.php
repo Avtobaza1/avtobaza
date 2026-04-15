c73a257fcc7911a4fc1e51c8fa74c1058ea3ca1c
Avtobaza1 <AutoMaint@proton.me>
2026-04-14 20:47:52 -0700

    Add files via upload

---
+++ snippets/autobaza---save-snippets.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * AutoBaza - Save
+ */
+add_filter('wpda_before_insert_row', function($data, $table) {
+    global $wpdb;
+
+    $current_user = wp_get_current_user();
+    $user_name    = $current_user->display_name;
+
+    if ($table === $wpdb->prefix . 'ab_maintenance_orders') {
+        $data['created_by'] = $user_name;
+    }
+
+    if ($table === $wpdb->prefix . 'ab_odometer_logs') {
+        $data['created_by'] = $user_name;
+    }
+
+    return $data;
+}, 10, 2);
